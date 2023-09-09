4d5c1f1de31295fc32bfaf5a9b43ab6025a12a3f
Chirag Jain <98633686+chiragjain16@users.noreply.github.com>
2023-09-09 10:39:11 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Login_form_on_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to the Login Form!");
+
+            Console.Write("Username: ");
+            string username = Console.ReadLine();
+
+            Console.Write("Password: ");
+            string password = Console.ReadLine();
+
+            // Check if the username and password are correct
+            if (username == "admin" && password == "admin")
+            {
+                Console.WriteLine("Login successful!");
+            }
+            else
+            {
+                Console.WriteLine("Invalid username or password");
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
